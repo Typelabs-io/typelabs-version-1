0b5469d7831ec470db680ba1008d663075664ff7
Michael <michaelasiedu148@gmail.com>
2023-03-15 16:30:10 +0000

    typelabs.io file setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Purple: "#8B00FF",
+        Lavender: "#CCCCFF",
+        Teal: "#39A5A5",
+        lightTeal: "#8FCACA",
+      },
+      fontFamily: {
+        secular: ["Secular One, sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
