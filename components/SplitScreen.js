7730ae84a65a62057eaed0ea59605785ff1c7780
Michael <michaelasiedu148@gmail.com>
2023-03-20 22:12:49 +0000

    RHS alter

--- components/SplitScreen.js
+++ components/SplitScreen.js
@@ -3,7 +3,7 @@ import React from "react";
 const SplitScreen = ({ left: Left, right: Right }) => {
   // props for the left and right hand will be passed in the App.jsx
   return (
-    <div className="px-4 md:px-8 lg:px-16 flex flex-col md:flex-row">
+    <div className="px-1 md:px-8 lg:px-16 flex flex-col md:flex-row">
       <div className=" w-full md:w-1/2 pr-4 md:border-r-2 md:border-accent hidden md:block">
         {/* Hidden on smaller devices */}
         <Left />
