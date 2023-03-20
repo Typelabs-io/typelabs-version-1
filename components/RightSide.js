60ecab9a617ccf92a8238ef9df728ddc6cd6e00c
Michael <michaelasiedu148@gmail.com>
2023-03-20 22:18:01 +0000

    RHS Alter

--- components/RightSide.js
+++ components/RightSide.js
@@ -4,13 +4,20 @@ const RightSide = () => {
   return (
     <div className="mona h-screen flex items-center">
       <div className="bg-white border-2 p-3 border-Reef200 md:p-6 rounded-lg mx-auto">
-        <p className="tracking-wider  text-md leading-normal">
+        <p className=" block tracking-wider text-justify text-md leading-loose md:hidden">
+          {" "}
+          We are currently in the process of building our agency, and are
+          excited to launch soon. If you are interested in our services and
+          would like to be notified when we launch, join our waitlist to get a{" "}
+          <span className="font-bold text-Reef200"> FREE QUOTE.</span>
+        </p>
+        <p className="hidden md:block tracking-wider  text-md leading-loose">
           {" "}
           We are currently in the process of building our agency, and
           <br />
           are excited to launch soon. If you are interested in our <br />
           services and would like to be notified when we launch, join <br />
-          our waitlist and get a{" "}
+          our waitlist to get a{" "}
           <span className="font-bold text-Reef200"> FREE QUOTE.</span>
         </p>
         <div class="mx-auto">
