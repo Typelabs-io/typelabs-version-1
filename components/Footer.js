dd8d3d5c7da40f72be140d2d1696ec63148f5fbb
7heMagician <obbengvincent@gmail.com>
2023-03-22 23:30:51 +0000

    created new footer for mobile

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-transparent py-2 px-4 tablet:px-8 desktop:px-16 z-10 desktop:w-1/2 absolute inset-x-0 bottom-0">
+    <footer className="bg-transparent py-2 px-4 tablet:px-8 desktop:px-16 hidden desktop:block">
       <div className="mx-auto">
         <p className="text-gray-400 mona text-caption text-center">
           © {new Date().getFullYear()} Typelabs. All rights reserved.
