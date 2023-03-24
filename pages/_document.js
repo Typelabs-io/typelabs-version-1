0d701a235562df59483a796a5db46552d3db3678
Michael <michaelasiedu148@gmail.com>
2023-03-24 23:48:36 +0000

    commented script tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Image from "next/image";
 
-
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,17 +8,15 @@ export default function Document() {
       <body className="bg-fixed bg-center bg-cover bg-[url('/background.svg')]">
         <Main />
         <NextScript />
-        <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+        {/* <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
         <noscript>
           <Image
             src="https://queue.simpleanalyticscdn.com/noscript.gif"
             alt=""
             referrerpolicy="no-referrer-when-downgrade"
           />
-        </noscript>
+        </noscript> */}
       </body>
     </Html>
   );
 }
-
-
