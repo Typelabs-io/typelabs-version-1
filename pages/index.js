3482642ffc70e07bbfc2c774938d7ab0ab3d4530
Michael <michaelasiedu148@gmail.com>
2023-03-25 14:14:24 +0000

    added one meta tag

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,7 @@ export default function Home() {
           property="twitter:image"
           content="https://elmerlar.sirv.com/BA4CE5EA-BC98-4672-9753-B03189D4D345.png"
         />
+        <meta name="twitter:domain" content="typelabs.io" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="icon" href="/favicon.ico" />
