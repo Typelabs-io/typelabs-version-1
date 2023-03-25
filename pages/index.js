56a9423fdb7de2902277ce83dc1ca691ecba2714
Michael <michaelasiedu148@gmail.com>
2023-03-25 13:02:51 +0000

    updated meta tags

--- pages/index.js
+++ pages/index.js
@@ -10,30 +10,23 @@ export default function Home() {
         <title>
           High-Quality Documentation for Software Products - Typelabs
         </title>
-        <meta name="description" content="Page description." />
         <meta
-          property="og:title"
-          content="High-Quality Documentation for Software Products - Typelabs"
+          name="description"
+          content="Our agency specializes in creating high-quality user manuals, online help systems, and articles for software products.Get competitive pricing without compromising on quality."
         />
+        <meta property="og:title" content="Typelabs" />
         <meta
           property="og:description"
-          content="Our agency specializes in creating high-quality user manuals, online help systems, and articles for software products.Get competitive pricing without compromising on quality."
+          content="High-Quality Documentation for Software Products "
         />
         <meta
           property="og:image"
           content="https://elmerlar.sirv.com/BA4CE5EA-BC98-4672-9753-B03189D4D345.png"
         />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta property="og:url" content="https://typelabs.io" />
-        {/* <meta name="twitter:card" content="summary" />
-        <meta
-          name="twitter:title"
-          content="High-Quality Documentation for Software Products - Typelabs"
-        />
-        <meta name="twitter:description" content="" />
-        <meta
-          name="twitter:image"
-          content="https://elmerlar.sirv.com/BA4CE5EA-BC98-4672-9753-B03189D4D345.png"
-        /> */}
+        <meta property="twitter:site" content="@TypelabsHQ" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
