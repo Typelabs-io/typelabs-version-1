e8888bee614eb29a750be1994f04cf02ab3ee5cd
Michael <michaelasiedu148@gmail.com>
2023-03-25 12:44:38 +0000

    added og image

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,11 @@ export default function Home() {
           property="og:description"
           content="Our agency specializes in creating high-quality user manuals, online help systems, and articles for software products.Get competitive pricing without compromising on quality."
         />
-        <meta property="og:image" content="" />
-        <meta property="og:url" content="" />
+        <meta
+          property="og:image"
+          content="https://elmerlar.sirv.com/BA4CE5EA-BC98-4672-9753-B03189D4D345.png"
+        />
+        <meta property="og:url" content="https://typelabs.io" />
         <meta name="twitter:card" content="summary" />
         <meta
           name="twitter:title"
@@ -30,7 +33,10 @@ export default function Home() {
           name="twitter:description"
           content="High-Quality Documentation for Software Products"
         />
-        <meta name="twitter:image" content="" />
+        <meta
+          name="twitter:image"
+          content="https://elmerlar.sirv.com/BA4CE5EA-BC98-4672-9753-B03189D4D345.png"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
