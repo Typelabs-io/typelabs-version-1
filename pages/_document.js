801bea5cc88e9679dc2f39de115e076fcc65bf37
Michael <michaelasiedu148@gmail.com>
2023-03-25 14:10:23 +0000

    added few meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,27 +4,7 @@ import Image from "next/image";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          name="description"
-          content="Our agency specializes in creating high-quality user manuals, online help systems, and articles for software products.Get competitive pricing without compromising on quality."
-        />
-        <meta property="og:title" content="Typelabs" />
-        <meta
-          property="og:description"
-          content="High-Quality Documentation for Software Products "
-        />
-        <meta
-          property="og:image"
-          content="https://elmerlar.sirv.com/BA4CE5EA-BC98-4672-9753-B03189D4D345.png"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-
-        <meta property="og:url" content="https://typelabs.io" />
-        <meta property="twitter:site" content="@TypelabsHQ" />
-
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <Head />
       <body className="bg-fixed bg-center bg-cover bg-[url('/background.svg')]">
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,34 @@ export default function Home() {
         <title>
           High-Quality Documentation for Software Products - Typelabs
         </title>
+        <meta
+          name="description"
+          content="Our agency specializes in creating high-quality user manuals, online help systems, and articles for software products.Get competitive pricing without compromising on quality."
+        />
+        <meta property="og:title" content="Typelabs" />
+        <meta
+          property="og:description"
+          content="High-Quality Documentation for Software Products "
+        />
+        <meta
+          property="og:image"
+          content="https://elmerlar.sirv.com/BA4CE5EA-BC98-4672-9753-B03189D4D345.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta property="og:url" content="https://typelabs.io" />
+        <meta property="twitter:site" content="@TypelabsHQ" />
+        <meta property="twitter:title" content="Typelabs" />
+        <meta
+          property="twitter:description"
+          content="High-Quality Documentation for Software Products "
+        />
+        <meta
+          property="twitter:image"
+          content="https://elmerlar.sirv.com/BA4CE5EA-BC98-4672-9753-B03189D4D345.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
